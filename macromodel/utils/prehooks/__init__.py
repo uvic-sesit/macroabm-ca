f807a552be9bacef449eeeea6d1ce5769b2f0864
devenazevedo <devenazevedo@uvic.ca>
2026-01-29 11:06:22 -0800

    Add pre/post hook energy price shock from CSV

--- macromodel/utils/prehooks/__init__.py
+++ macromodel/utils/prehooks/__init__.py
@@ -4,8 +4,14 @@ This module provides factory functions to create pre-hooks that can be registere
 with a Simulation object to perform interventions at specific timesteps.
 """
 
+from macromodel.utils.prehooks.energy_price_shock import (
+    create_energy_price_shock_hook,
+)
 from macromodel.utils.prehooks.productivity_subsidy import (
     create_productivity_subsidy_hook,
 )
 
-__all__ = ["create_productivity_subsidy_hook"]
+__all__ = [
+    "create_energy_price_shock_hook",
+    "create_productivity_subsidy_hook",
+]
